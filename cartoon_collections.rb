04cac16680e437921e86e89e0c906156c76b8ee8
Thomas Donovan <tdonovan79@gmail.com>
2019-11-11 02:17:08 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,33 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+#puts each dwarf in array
+def roll_call_dwarves(dwarf_array)
+  dwarf_array.each_with_index{|dwarf, index|
+  puts "#{index + 1}. #{dwarf}\n"
+  }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+#turn each element into a captain planet call
+def summon_captain_planet(planeteers_calls)
+  return planeteers_calls.collect{|x| x.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+#return true if there is a word over four fase otherwise
+def long_planeteer_calls(calls)
+  short_flag = false
+  calls.each do |x|
+    if x.length > 4
+      short_flag = true
+    end
+  end
+  return short_flag
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+#return first type fo cheese that appears in array
+def find_the_cheese(food_array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  food_array.each do |food|
+    if cheese_types.include?(food)
+      return food
+    end
+  end
+  return nil
 end
